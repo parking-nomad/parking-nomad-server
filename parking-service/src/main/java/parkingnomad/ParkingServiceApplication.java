4b63d32086f6e03dea61db3bf1981b0cbafb8782
dev_kong <ahdjd5@gmail.com>
2024-03-16 18:17:15 +0900

    feat: parking service init

---
+++ parking-service/src/main/java/parkingnomad/ParkingServiceApplication.java
@@ -0,0 +1,16 @@
+package parkingnomad;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableDiscoveryClient
+public class ParkingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingServiceApplication.class, args);
+    }
+}
